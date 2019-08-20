f7ed8adf41bb7215487875d2f42cdc0e6941bc7c
Beigenut <beigenut@gmail.com>
2019-08-20 15:11:10 -0400

    Set initial react project Install create-react-app axios

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import ResourceList from './ResourceList.js';
 
-function App() {
+const App = () => {
+  const [resourceName, setResourceName] = useState('posts');
+  // const arr = [1, 2]
+  // const [first, second] = arr 
+  // console.log(first) = 1
+  // console.log(arr[1]) = 2
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => setResourceName('posts')}>Posts</button>
+      <button onClick={() => setResourceName('todos')}>Todos</button>
+      <ResourceList resourceName={resourceName} />
     </div>
   );
 }
---
+++ src/ResourceList.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ResourceList = (props) => {
+  return (
+    <div>{props.resourceName}</div>
+  )
+}
+
+export default ResourceList
